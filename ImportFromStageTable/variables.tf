bbc7620ebe8c17d292fd386d99cdf5bd0475c8e3
Abdiel Sanchez <asanchez@transactrx.com>
2023-05-04 09:37:53 -0400

    Expose variable for task interval

--- ImportFromStageTable/variables.tf
+++ ImportFromStageTable/variables.tf
@@ -32,4 +32,10 @@ variable "raw_table_name" {
 variable "sql_import_query" {
   description = "Query to populate the desired table from the stream.  This will most likely be a MERGE query."
   type        = string
-} 
+}
+
+variable "import_interval" {
+  description = "Interval to run task to import the data."
+  type = string
+  default = "60 MINUTE"
+}
