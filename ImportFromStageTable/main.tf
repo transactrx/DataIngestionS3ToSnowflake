e893341ed5c4b65c6310bc29b352c330c4165d2d
Abdiel Sanchez <asanchez@transactrx.com>
2023-05-05 11:40:31 -0400

    Made code serverless and stop creating warehouses.

--- ImportFromStageTable/main.tf
+++ ImportFromStageTable/main.tf
@@ -15,13 +15,13 @@ locals {
   import_stored_proc_name = upper("stream_import_sp_${var.name}")
 }
 
-resource "snowflake_warehouse" "data_load_warehouse" {
-  name                = local.warehouse_name
-  warehouse_size      = var.warehouse_size
-  auto_suspend        = 60
-  auto_resume         = true
-  initially_suspended = true
-}
+# resource "snowflake_warehouse" "data_load_warehouse" {
+#   name                = local.warehouse_name
+#   warehouse_size      = var.warehouse_size
+#   auto_suspend        = 60
+#   auto_resume         = true
+#   initially_suspended = true
+# }
 
 resource "snowflake_stream" "transactions_stream" {
   name        = local.stream_name
@@ -45,11 +45,6 @@ resource "snowflake_procedure" "data_load_sp" {
   language = "JAVASCRIPT"
   comment = "Store Procedure to load new data comming the transactions table.  It will create the WH if it does not exist and suspend it after execution."
   execute_as = "CALLER"
-  
-  arguments {
-    name = "warehouse_name"
-    type = "VARCHAR"
-  }
 
   statement = <<-EOT
       sql_command = "select system$stream_has_data('${var.database_name}.${var.schema_name}.${snowflake_stream.transactions_stream.name}');";
@@ -59,17 +54,7 @@ resource "snowflake_procedure" "data_load_sp" {
       var has_data = rs.getColumnValue(1);
 
       if (has_data) {
-        var whn = WAREHOUSE_NAME
-        sql_command = "CREATE WAREHOUSE IF NOT EXISTS " + whn + " WITH WAREHOUSE_SIZE = 'LARGE' INITIALLY_SUSPENDED=FALSE AUTO_SUSPEND=60 AUTO_RESUME=TRUE;";
-
-        stmt = snowflake.createStatement({ sqlText: sql_command });
-        stmt.execute();
-
         sql_command = ${var.sql_import_query}
-        stmt = snowflake.createStatement({ sqlText: sql_command });
-        stmt.execute();
-
-        sql_command = "ALTER WAREHOUSE " + whn + " SUSPEND;";
         stmt = snowflake.createStatement({ sqlText: sql_command });
         stmt.execute();
 
@@ -85,17 +70,18 @@ resource "snowflake_task" "stream_task" {
   name      = local.stream_task_name
   database  = var.database_name
   schema    = var.schema_name
-  warehouse = local.warehouse_name
+  # warehouse = local.warehouse_name
 
   user_task_timeout_ms = "3600000" # 1 hour
   comment   = "Load powerline data from external stage to table every hour."
   enabled   = true
   schedule  = var.import_interval
 
-  sql_statement = "CALL ${var.database_name}.${var.schema_name}.${snowflake_procedure.data_load_sp.name}('${local.warehouse_name}')"
+  # sql_statement = "CALL ${var.database_name}.${var.schema_name}.${snowflake_procedure.data_load_sp.name}('${local.warehouse_name}')"
+  sql_command = ${var.sql_import_query}
 
   depends_on = [
-    snowflake_procedure.data_load_sp,
-    snowflake_warehouse.data_load_warehouse
+    snowflake_procedure.data_load_sp
+    # snowflake_warehouse.data_load_warehouse
   ]
 }
--- S3ToStageTable/main.tf
+++ S3ToStageTable/main.tf
@@ -52,61 +52,62 @@ resource "snowflake_table" "transactions_table" {
   }
 }
 
-resource "snowflake_warehouse" "data_load_warehouse" {
-  name                = local.warehouse_name
-  warehouse_size      = var.warehouse_size
-  auto_suspend        = 60
-  auto_resume         = true
-  initially_suspended = true
-}
+# resource "snowflake_warehouse" "data_load_warehouse" {
+#   name                = local.warehouse_name
+#   warehouse_size      = var.warehouse_size
+#   auto_suspend        = 60
+#   auto_resume         = true
+#   initially_suspended = true
+# }
 
-resource "snowflake_procedure" "data_load_sp" {
-  name      = local.import_stored_proc_name
-  database  = var.database_name
-  schema    = var.schema_name
-  return_type = "VARCHAR"
-  language = "JAVASCRIPT"
-  comment = "Store Procedure to load new data comming from the S3 bucket and suspending the WH after execution"
-  execute_as = "CALLER"
+# resource "snowflake_procedure" "data_load_sp" {
+#   name      = local.import_stored_proc_name
+#   database  = var.database_name
+#   schema    = var.schema_name
+#   return_type = "VARCHAR"
+#   language = "JAVASCRIPT"
+#   comment = "Store Procedure to load new data comming from the S3 bucket and suspending the WH after execution"
+#   execute_as = "CALLER"
   
-  arguments {
-    name = "warehouse_name"
-    type = "VARCHAR"
-  }
+#   arguments {
+#     name = "warehouse_name"
+#     type = "VARCHAR"
+#   }
 
-  statement = <<-EOT
-      var whn = WAREHOUSE_NAME
-      sql_command = "CREATE WAREHOUSE IF NOT EXISTS " + whn + " WITH WAREHOUSE_SIZE = 'LARGE' INITIALLY_SUSPENDED=FALSE AUTO_SUSPEND=60 AUTO_RESUME=TRUE;";
+#   statement = <<-EOT
+#       var whn = WAREHOUSE_NAME
+#       sql_command = "CREATE WAREHOUSE IF NOT EXISTS " + whn + " WITH WAREHOUSE_SIZE = 'LARGE' INITIALLY_SUSPENDED=FALSE AUTO_SUSPEND=60 AUTO_RESUME=TRUE;";
 
-      stmt = snowflake.createStatement({ sqlText: sql_command });
-      stmt.execute();
+#       stmt = snowflake.createStatement({ sqlText: sql_command });
+#       stmt.execute();
 
-      sql_command = "COPY INTO ${snowflake_table.transactions_table.name} FROM (SELECT * FROM @${snowflake_stage.external_stage.name})";
-      stmt = snowflake.createStatement({ sqlText: sql_command });
-      stmt.execute();
+#       sql_command = "COPY INTO ${snowflake_table.transactions_table.name} FROM (SELECT * FROM @${snowflake_stage.external_stage.name})";
+#       stmt = snowflake.createStatement({ sqlText: sql_command });
+#       stmt.execute();
 
-      sql_command = "ALTER WAREHOUSE " + whn + " SUSPEND;";
-      stmt = snowflake.createStatement({ sqlText: sql_command });
-      stmt.execute();
+#       sql_command = "ALTER WAREHOUSE " + whn + " SUSPEND;";
+#       stmt = snowflake.createStatement({ sqlText: sql_command });
+#       stmt.execute();
 
-      return "Data loaded successfully";
-  EOT
-}
+#       return "Data loaded successfully";
+#   EOT
+# }
 
 resource "snowflake_task" "data_load_task" {
   name      = local.data_load_task_name
   database  = var.database_name
   schema    = var.schema_name
-  warehouse = local.warehouse_name
+  # warehouse = local.warehouse_name
   # This can be a CRON or an interval in minutes
   schedule        = var.data_load_interval
   user_task_timeout_ms = "3600000" # 1 hour
   comment         = "Load powerline data from external stage to table every hour."
   enabled = true
 
-  sql_statement = "CALL ${var.database_name}.${var.schema_name}.${snowflake_procedure.data_load_sp.name}('${local.warehouse_name}')"
+  # sql_statement = "CALL ${var.database_name}.${var.schema_name}.${snowflake_procedure.data_load_sp.name}('${local.warehouse_name}')"
+  sql_command = "COPY INTO ${snowflake_table.transactions_table.name} FROM (SELECT * FROM @${snowflake_stage.external_stage.name})";
 
-  depends_on = [
-    snowflake_procedure.data_load_sp
-  ]
+  # depends_on = [
+  #   snowflake_procedure.data_load_sp
+  # ]
 }
