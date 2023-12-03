328bd47d0fca63891b6779e064030032863b53e7
melaraj2 <melaraj@gmail.com>
2023-12-03 13:19:59 -0500

    Update outputs.tf

--- S3ToStageTable/outputs.tf
+++ S3ToStageTable/outputs.tf
@@ -1,14 +1,14 @@
-output "data_load_task_full_name" {
-  value       = "${snowflake_task.data_load_task.database}.${snowflake_task.data_load_task.schema}.${snowflake_task.data_load_task.name}"
-  description = "Full name (with database and schema) of the task which loads the S3 data into the transactions table."
-}
+#output "data_load_task_full_name" {
+#  value       = "${snowflake_task.data_load_task.database}.${snowflake_task.data_load_task.schema}.${snowflake_task.data_load_task.name}"
+#  description = "Full name (with database and schema) of the task which loads the S3 data into the transactions table."
+#}
 
 output "stage_table_name" {
   value       = snowflake_table.transactions_table.name
   description = "Transactions table name."
 }
 
-output "stage_table_full_name" {
-  value       = "${snowflake_task.data_load_task.database}.${snowflake_task.data_load_task.schema}.${snowflake_table.transactions_table.name}"
-  description = "Transactions table full name (including the database and schema)."
-}
+#output "stage_table_full_name" {
+#  value       = "${snowflake_task.data_load_task.database}.${snowflake_task.data_load_task.schema}.${snowflake_table.transactions_table.name}"
+#  description = "Transactions table full name (including the database and schema)."
+#}
