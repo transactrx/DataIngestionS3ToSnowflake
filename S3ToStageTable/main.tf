1a1af50199113d496eac1492637f1762aae77e36
DlopezTransactRx <dlopez@transactrx.com>
2025-06-13 18:49:50 -0400

    Relax snowflake provider version to support upgrade

--- S3ToStageTable/main.tf
+++ S3ToStageTable/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "snowflakedb/snowflake"
-      version = "~> 0.92.0"
+      version = ">= 0.92.0"
     }
     aws = {
       source  = "hashicorp/aws"
