39877f764d3019885abcc3a2e931df67ce151a17
Fernando Azouth <fazouth@transactrx.com>
2026-01-27 12:03:52 -0500

    Fix output names to match resource names being returned
    The output properties were accidentally swapped, causing the task name
    output to return the stream name, while the stream name output returned
    the task name.

--- ImportFromStageTable/outputs.tf
+++ ImportFromStageTable/outputs.tf
@@ -1,9 +1,9 @@
-output "task_name" {
+output "stream_name" {
   description = "Name of the Snowflake stream created"
   value       = snowflake_stream_on_table.transactions_stream.fully_qualified_name
 }
 
-output "stream_name" {
+output "task_name" {
   description = "Name of the Snowflake task created"
   value       = snowflake_task.stream_task.fully_qualified_name
 }
